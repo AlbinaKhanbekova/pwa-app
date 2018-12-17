4f19a205ebbabd8cc50fc5eb034c419aceba99d5
Albina Khanbekova <albina.khanbekova@gmail.com>
2018-12-17 08:54:07 +0300

    Updates

---
+++ precache-manifest.18096a66576eb7d7bf20b24b33e789f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "89bf7b874f2df08752b4",
+    "url": "/pwa-app/static/js/runtime~main.89bf7b87.js"
+  },
+  {
+    "revision": "6b6cc76d6215ecb8d9fe",
+    "url": "/pwa-app/static/js/main.6b6cc76d.chunk.js"
+  },
+  {
+    "revision": "1d597ccb0deef67ae028",
+    "url": "/pwa-app/static/js/1.1d597ccb.chunk.js"
+  },
+  {
+    "revision": "6b6cc76d6215ecb8d9fe",
+    "url": "/pwa-app/static/css/main.1246c9f5.chunk.css"
+  },
+  {
+    "revision": "e8eea370167acac0b1f25ce235b0ccb3",
+    "url": "/pwa-app/index.html"
+  }
+];
